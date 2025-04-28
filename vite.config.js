56f490b2bcdf90be2f9580e3cc292847f4fcf2af
DmitriiPC <68027431+dumkaguv@users.noreply.github.com>
2025-04-28 11:56:33 +0300

    build for GH pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import htmlMinify from "vite-plugin-html-minify";
 
 export default defineConfig({
+  base: "./",
   plugins: [
     htmlMinify({
       collapseWhitespace: true,
